20f59c05059f75d850052b37e8228f69650f9f8e
arifyrlmaz <arifyorulmaz5@gmail.com>
2024-08-07 23:48:33 +0300

    roll dice app

---
+++ first_app/lib/dice_roller.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import "dart:math";
+
+class DiceRoller extends StatefulWidget {
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+final randomizer = Random();
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 2;
+  var currentDiceRoll2 = 2;
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+      currentDiceRoll2 = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          "assets/images/dice-$currentDiceRoll.png",
+          width: 200,
+        ),
+        Image.asset(
+          "assets/images/dice-$currentDiceRoll2.png",
+          width: 200,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+              padding: const EdgeInsets.only(top: 20),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 28)),
+          child: Text("Roll Dice"),
+        )
+      ],
+    );
+  }
+}
